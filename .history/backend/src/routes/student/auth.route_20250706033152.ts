22873349d23d22f87ee089be0844b16575b36e80
sujan0629 <sujanbhatta0629@gmail.com>
2025-07-12 01:07:00 +0545

    initial commit

--- .history/backend/src/routes/student/auth.route_20250706033152.ts
+++
@@ -1,9 +0,0 @@
-import { loginStudent, signUpStudent } from "controller/student/auth.controller";
-import { Router } from "express";
-
-const router = Router();
-
-router.post("/signup", signUpStudent);
-router.post("/login", loginStudent);
-
-export { router as StudentAuthRoute }
